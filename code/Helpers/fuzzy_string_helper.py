eae5a5fc1a2b2471a244ac068a5ca0bd6701ae33
Kelvyn <KelvynSouza>
2020-11-05 15:44:57 -0300

    Helper com funcionalidades fuzzy string matching, ocr e preocessamento de imagens. Alem do txt com as dependencias

---
+++ code/Helpers/fuzzy_string_helper.py
@@ -0,0 +1,49 @@
+from fuzzywuzzy import fuzz, process
+
+Str1 = "Apple Inc."
+Str2 = "apple Inc"
+Ratio = fuzz.ratio(Str1.lower(), Str2.lower())
+print(Ratio) # Result 95
+
+
+Str1 = "Los Angeles Lakers"
+Str2 = "Lakers"
+Ratio = fuzz.ratio(Str1.lower(), Str2.lower())
+Partial_Ratio = fuzz.partial_ratio(Str1.lower(), Str2.lower())
+print(Ratio) # Result 50
+print(Partial_Ratio) # Result 100
+
+
+# strings comparison the same, but they are in a different order
+Str1 = "united states v. nixon"
+Str2 = "Nixon v. United States"
+Ratio = fuzz.ratio(Str1.lower(), Str2.lower())
+Partial_Ratio = fuzz.partial_ratio(Str1.lower(), Str2.lower())
+Token_Sort_Ratio = fuzz.token_sort_ratio(Str1, Str2)
+print(Ratio) # Result 59
+print(Partial_Ratio) # Result 74
+print(Token_Sort_Ratio) # Result 100
+
+
+# these two strings are of widely differing lengths
+Str1 = "The supreme court case of Nixon vs The United States"
+Str2 = "Nixon v. United States"
+Ratio = fuzz.ratio(Str1.lower(), Str2.lower())
+Partial_Ratio = fuzz.partial_ratio(Str1.lower(), Str2.lower())
+Token_Sort_Ratio = fuzz.token_sort_ratio(Str1, Str2)
+Token_Set_Ratio = fuzz.token_set_ratio(Str1, Str2)
+print(Ratio)
+print(Partial_Ratio)
+print(Token_Sort_Ratio)
+print(Token_Set_Ratio)
+
+# get the string with the highest score
+str2Match = "apple inc"
+strOptions = ["Apple Inc.", "apple park", "apple incorporated", "iphone"]
+Ratios = process.extract(str2Match, strOptions)
+print(Ratios)
+# You can also select the string with the highest matching percentage
+highest = process.extractOne(str2Match, strOptions)
+print(highest)
+# [('Apple Inc.', 100), ('apple incorporated', 90), ('apple park', 67), ('iphone', 30)]
+# ('Apple Inc.', 100)
--- code/main.py
+++ code/main.py
@@ -1,64 +1,9 @@
-import re
 import cv2
-from pytesseract import Output, pytesseract
+import Helpers.image_ocr_helper as ocr_helper
+import Helpers.image_processing_helper as processing_helper
 
 img = cv2.imread('../Support/ImagesTest/OCR_Test2.png')
 
-
-# custom config for tesseract
-# custom_config = r'--oem 3 --psm 6 outputbase digits'
-# custom_config = r'-c tessedit_char_whitelist=abcdefghijklmnopqrstuvwxyz --psm 6' pegar apenas essas letras
-# custom_config = r'-c tessedit_char_blacklist=abcdefghijklmnopqrstuvwxyz --psm 6' Qualquer coisa menos essas letras
-# custom_config = r' -c preserve_interword_spaces=1 -l jpn --psm 6' tratar espaços gerados pelo ocr
-# print(pytesseract.image_to_string(img, config=custom_config))
-
-def get_characters_and_outline_positions(image):
-    custom_config = r'-l jpn+eng --psm 6'
-    h, w, c = image.shape
-    boxes = pytesseract.image_to_boxes(image, config=custom_config)
-    for b in boxes.splitlines():
-        b = b.split(' ')
-        img_result = cv2.rectangle(img, (int(b[1]), h - int(b[2])), (int(b[3]), h - int(b[4])), (0, 255, 0), 1)
-
-    cv2.imshow('img', img_result)
-    cv2.waitKey(0)
-
-
-def get_characters_to_string(image):
-    custom_config = r'-c preserve_interword_spaces=1 -l jpn --psm 6'
-    result = pytesseract.image_to_string(image, config=custom_config)
-    print(result)
-
-
-def get_words_and_outline_positions(image):
-    d = pytesseract.image_to_data(image, output_type=Output.DICT)
-    n_boxes = len(d['text'])
-    for i in range(n_boxes):
-        if int(d['conf'][i]) > 60:
-            (x, y, w, h) = (d['left'][i], d['top'][i], d['width'][i], d['height'][i])
-            image = cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
-
-    cv2.imshow('img', image)
-    cv2.waitKey(0)
-
-
-def get_text_from_image_with_regex(image):
-    d = pytesseract.image_to_data(image, output_type=Output.DICT)
-    keys = list(d.keys())
-
-    regex_pattern = '^(0[1-9]|[12][0-9]|3[01])/(0[1-9]|1[012])/(19|20)\d\d$'
-    regex_pattern = '/japanese/gim'
-
-    n_boxes = len(d['text'])
-    for i in range(n_boxes):
-        if int(d['conf'][i]) > 60:
-            if re.match(regex_pattern, d['text'][i]):
-                (x, y, w, h) = (d['left'][i], d['top'][i], d['width'][i], d['height'][i])
-                image = cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
-
-    cv2.imshow('img', image)
-    cv2.waitKey(0)
-
-
 if __name__ == '__main__':
-    get_characters_to_string(img)
+    image = processing_helper.get_grayscale(img)
+    ocr_helper.get_characters_to_string(image)
